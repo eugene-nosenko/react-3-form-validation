00ed2c220556b042b0fb0cbbfef3b1a19993a705
eugene-finn <1287175@gmail.com>
2019-08-05 22:22:46 +0300

    does not work

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,2 +1,11 @@
+import React, { Component } from 'react';
 export { default } from './Form';
+// import "./Form.css";
 
+export class Form extends React.Component {
+
+  render() {
+    return <div></div>
+  }
+
+}
